3bb503eff42cca272e48240dffb8aca5edfcee0b
gutzofter <gutzofter@alephchain.com>
2019-04-07 15:49:39 -0700

    adding views

--- routes/work_orders.js
+++ routes/work_orders.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gutzofter', msg: 'Good day!' });
+    res.render('work_orders', { title: 'Work Orders', msg: 'Good day!' });
 });
 
 module.exports = router;
