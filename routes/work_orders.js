16c76b409de7b13379e253dc71b9241b1b1e18a2
gutzofter <gutzofter@alephchain.com>
2019-04-08 21:05:59 -0700

    added table

--- routes/work_orders.js
+++ routes/work_orders.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var wo = [
+ {
+     number: 'AZ123',
+     product_type: '4oz Amber Glass',
+     output: 20
+ }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('work_orders', { title: 'Work Orders', msg: 'Good day!' });
+    res.render('work_orders', { title: 'Work Orders', msg: 'Good day!', work_orders = wo });
 });
 
 module.exports = router;
